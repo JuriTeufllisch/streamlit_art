90b58a8b84fac4a259aff2f55f14dee9e21f30a5
JuriTeufllisch <48556530+JuriTeufllisch@users.noreply.github.com>
2024-06-01 08:23:58 +0200

    Update art_app.py

--- art_app.py
+++ art_app.py
@@ -11,7 +11,7 @@ st.header('- simple, but unique -')
 st.sidebar.header('Input parameters')
 
 n = st.sidebar.slider('image width in symbols', min_value=1, max_value=200)
-image_size = st.sidebar.slider('image width in pixels', min_value=2, max_value=5000)
+image_size = st.sidebar.slider('image width in pixels', min_value=2, max_value=4000)
 
 lower_size = st.sidebar.slider('lower size of symbols', min_value=1, max_value=int((image_size/n)**2))
 upper_size = st.sidebar.slider('upper size of symbols', min_value=2, max_value=int(2*(image_size/n)**2))
